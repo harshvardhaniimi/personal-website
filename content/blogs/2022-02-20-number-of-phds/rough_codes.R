552ddcfd185df7c378478e452f3eae96903076e3
harshvardhaniimi <hvsc1708@gmail.com>
2022-04-10 20:50:06 -0400

    y

--- content/blogs/2022-02-20-number-of-phds/rough_codes.R
+++ content/blogs/2022-02-20-number-of-phds/rough_codes.R
@@ -17,11 +17,22 @@ phds_top_engineering = phds %>%
   summarise(n_phds = sum(n_phds)) %>% 
   filter(n_phds > 100) %>% 
   slice_max(order_by = n_phds, n = 6)
-  
+
+phds_top_engineering
   
 
 phds_top_engineering %>%
 ggplot(aes(x = year, y = n_phds, fill = field)) + 
   geom_bar(stat = "identity") + 
-  facet_wrap( ~ variable) + 
+  facet_wrap( ~ field) + 
   scale_x_continuous(breaks = seq(1, 12, 2))
+
+
+phds %>% 
+  filter(field %in% phds_top_engineering$field) %>% 
+ggplot(aes(x = year, y = n_phds, fill = field)) +
+  geom_bar(stat = "identity") + 
+  scale_x_continuous(labels = scales::label_number(accuracy = 1)) +
+  scale_fill_manual(values = MetBrewer::met.brewer("Hokusai1", 6)) +
+  facet_wrap( ~ field) +
+  labs(x = "Year", y = "Number of PhDs", fill = "Field")
